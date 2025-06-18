bf0cdd7f04debd853570b03ff3945e0afb5ff167
Aster <aster@Asters-MacBook-Pro.local>
2025-06-18 17:41:54 -0600

    Add Cascading rules, add delete and WIP blur

--- src/components/ExerciseList/ExerciseList.tsx
+++ src/components/ExerciseList/ExerciseList.tsx
@@ -6,7 +6,17 @@ import { Fetcher } from 'swr';
 import useSWR from 'swr';
 import { Button } from '../ui/button';
 import { Card, CardContent } from '../ui/card';
-import { Trash2 } from 'lucide-react';
+import { Settings2, Trash2 } from 'lucide-react';
+import { Input } from '../ui/input';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 const fetcher: Fetcher<ExerciseListItemType[], string> = (url) =>
   fetch(url).then(res => {
@@ -18,6 +28,8 @@ function ExerciseList({ exercises }: { exercises: ExerciseListItemType[] }) {
   const [exName, setExName] = React.useState<string>('');
   const { data, error, isLoading, mutate } = useSWR<ExerciseListItemType[]>(`/api/exercises`, fetcher, { fallbackData: exercises })
 
+  const [canEdit, setCanEdit] = React.useState<string | undefined>();
+  const [editedName, setEditedName] = React.useState<string>('');
 
   async function deleteExercise(id: string) {
 
@@ -77,19 +89,29 @@ function ExerciseList({ exercises }: { exercises: ExerciseListItemType[] }) {
   return <div className='mx-auto max-w-3xl'>
 
     <div className='w-full flex flex-col gap-5'>
-      {data?.map(({ id, name, muscle_groups }: ExerciseListItemType) => (
+      {data?.map(({ id, name }: ExerciseListItemType) => (
         <Card key={id} className='w-full' >
           <CardContent className='flex'>
-            <span> {name}</span> <span> {JSON.stringify(muscle_groups)}  </span>
-            <Trash2 width={24} height={24} onClick={() => deleteExercise(id)} className='ml-auto cursor-pointer' />
+            <span> <input name="exerciseName" onChange={(e) => setEditedName(e.target.value)} value={canEdit == id ? editedName : name} readOnly={canEdit !== id} className='bg-transparent outline-0 border-0 p-0 m-0 focus:outline-none focus:ring-0' /> </span>
+            <DropdownMenu>
+              <DropdownMenuTrigger className='ml-auto shrink-0 cursor-pointer'>
+                <Settings2 />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>Options</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={() => { setCanEdit(id), setEditedName(name) }}>Edit</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => { deleteExercise(id) }}>Delete</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </CardContent>
         </Card>
       ))}
 
     </div>
-    <form onSubmit={(e) => handlSubmit(e)}>
+    <form className='mt-4 flex flex-col gap-3 ' onSubmit={(e) => handlSubmit(e)}>
       <label>
-        <input required id="exerciseName" name="exerciseName" value={exName} onChange={(e) => setExName(e.target.value)} type='text' placeholder='Bench Press' />
+        <Input required id="exerciseName" name="exerciseName" value={exName} onChange={(e) => setExName(e.target.value)} type='text' placeholder='Bench Press' />
       </label>
       <Button className='w-full'>Create </Button>
     </form>
